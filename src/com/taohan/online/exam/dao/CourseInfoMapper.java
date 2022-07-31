9f5765dc1e53985301885d41473984fa33afc482
xunshe <1391914734@qq.com>
2022-07-31 22:41:43 +0800

    f

---
+++ src/com/taohan/online/exam/dao/CourseInfoMapper.java
@@ -0,0 +1,30 @@
+package com.taohan.online.exam.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.taohan.online.exam.po.CourseInfo;
+
+/**
+  *
+  * <p>Title: CourseInfoMapper</p>
+  * <p>Description: </p>
+  * @author: taohan
+  * @time: 下午5:23:23
+  * @version: 1.0
+  */
+
+@Repository
+public interface CourseInfoMapper {
+
+	public List<CourseInfo> getCourses(CourseInfo course);
+	
+	public int isUpdateCourse(CourseInfo course);
+
+	public int isAddCourse(CourseInfo course);
+	
+	public int isDelCourse(int courseId);
+	
+	public CourseInfo getCourseById(int courseId);
+}
